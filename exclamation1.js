002edbe3c8f1f699e4bd9da6886b8e79684690bb
Kristin Sztengel <kcsztengel@gmail.com>
2017-04-01 08:53:09 -0600

    exclamation point #1 in series

---
+++ exclamation1.js
@@ -0,0 +1,10 @@
+8 kyu
+
+Exclamation marks series #1: Remove a exclamation mark from the end of string
+JavaScript:
+
+function remove(s){
+ let newS = s.replace(/!$/, "")
+return newS
+}
+1 minute agoRefactorDiscuss
