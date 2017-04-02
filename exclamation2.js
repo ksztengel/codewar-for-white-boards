a58c6ac9eac34be4bf7c54ccaf9d25e345ffa37d
Kristin Sztengel <kcsztengel@gmail.com>
2017-04-02 17:26:39 -0600

    exlamation exercise

---
+++ exclamation2.js
@@ -0,0 +1,12 @@
+8 kyu
+Exclamation marks series #2:
+
+Remove all exclamation marks from the end of sentence
+JavaScript:
+
+function remove(s){
+
+return s.replace(/!+$/, "")
+
+}
+9 hours agoRefactorDiscuss
